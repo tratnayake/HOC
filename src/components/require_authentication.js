a6e9f7507246e362e4b405935472efdec2a3143d
Thilina Ratnayake <thilina@tratnayake.me>
2017-09-26 21:44:13 -0700

    Finishing HOC's

---
+++ src/components/require_authentication.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent){
+  class Authentication extends Component{
+    render(){
+      console.log(this.props.authenticated);
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state){
+    return { authenticated: state.authenticated };
+  }
+  return connect(mapStateToProps)(Authentication);
+}
---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return(
+    <div>
+    SUper Special Secret Recipe
+    <ul>
+      <li>1 Cup Sugar</li>
+      <li>1 Cup Pepper</li>
+      <li>1 Cup Salts</li>
+    </ul>
+    </div>
+  )
+}
