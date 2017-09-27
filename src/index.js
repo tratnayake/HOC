5f9e12bd102492fd91a8832f1888f67459bdb103
Thilina Ratnayake <tratnayake@my.bcit.ca>
2017-09-26 22:28:37 -0700

    more comments`

--- src/index.js
+++ src/index.js
@@ -13,8 +13,14 @@ import reducers from './reducers';
 //Import Higher Order Components(HOC's)
 import requireAuth from './components/require_authentication';
 
+//Include middleware (none for this project)
+// Create the Redux data store (don't worry about this for now)
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+//BrowserHistory is what keeps track of the URL (i.e. localhost:8080/** <-
+//everything after slash)
+
+//{ } 's are JSX so injecting our components.
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
